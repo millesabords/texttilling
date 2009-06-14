614eef874a582bd7509f9841c3b754157e8b59e0
stephane.campinas <stephane.campinas@479eebda-5811-11de-b31f-81d54249e7d9>
2009-06-14 14:37:21 +0000

    updated the calcul of idf

--- trunk/main.py
+++ trunk/main.py
@@ -2,11 +2,17 @@ from lemmatizer import *
 from similarite import *
 from borders import *
 
-tokenizer = Tokenizer(['heracles.txt'], 3)
+tokenizer = Tokenizer(['bible.txt'], 3)
 tokens = tokenizer.preprocess()
+
+print "lemmatizer finished"
+print
 
 sim = similarity(tokens)
 sim.tfidf()
+
+print 
+print "similarity calculs finished"
 
 B = borders(tokens, sim)
 B.test()
